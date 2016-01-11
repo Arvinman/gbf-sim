8ae0a48544556f624022871184369f5d3eb622d4
Pornvibool Tippayawat <pornvibool.t@gmail.com>
2016-01-11 17:26:16 +0700

    v0.3 - rollUntilFound

--- controller/gbfSim.controller.js
+++ controller/gbfSim.controller.js
@@ -15,6 +15,8 @@
         $scope.totalSsr;
         $scope.totalRoll = 100;
         $scope.click = onClick;
+        $scope.rollUntilFound = rollUntilFound;
+        $scope.clearAllChecked = clearAllChecked;
 
         $scope.gachaDatabase;
         
@@ -50,6 +52,30 @@
             }
         };
 
+        function rollUntilFound() {
+            if (!isAnyChecked())
+                return;
+                
+            resetGachaGet();
+            $scope.totalRoll = 0;
+            var found = false;
+            while (!found) {
+                ++$scope.totalRoll;
+                var rollResult = rollGacha($scope.ssrRate);
+                if (rollResult.drop == "SSR") {
+                    ++$scope.totalSsr;
+
+                    var roll = rngService.integer(0, $scope.gachaDatabase.length - 1);
+                    ++$scope.gachaDatabase[roll].totalGet;
+                    rollResult.name = $scope.gachaDatabase[roll].name;
+
+                    if ($scope.gachaDatabase[roll].checked)
+                        found = true;
+                }
+                $scope.rollList.push(rollResult);
+            }
+        };
+
         function rollGacha(ssrRate) {
             var roll = rngService.floating(0, 1);
             if (roll < ssrRate) {
@@ -57,8 +83,20 @@
             } else {
                 return { drop: "miss", roll: roll };
             }
+        };
+
+        function isAnyChecked() {
+            for (var i = 0; i < $scope.gachaDatabase.length; ++i)
+                if ($scope.gachaDatabase[i].checked)
+                    return true;
+
+            return false;
         }
 
+        function clearAllChecked (){
+            for (var i = 0; i < $scope.gachaDatabase.length; ++i)
+                $scope.gachaDatabase[i].checked = false;
+        }
     }
 
 })(window.angular);
